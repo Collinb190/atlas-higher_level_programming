a4b261805ab4cf883999761b539c2b79426175f0
Collinb190 <collinb190@gmail.com>
2024-02-17 23:59:53 -0600

    5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- Prints the following description of the table in your MySQL server.
-SHOW CREATE TABLE 'first_table';
+SELECT CONCAT('Table: ', TABLE_NAME. '\n', 'Create Table', CREATE_STATEMENT)
+FROM information_schema.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
