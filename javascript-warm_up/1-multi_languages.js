2a906396b4d8282111661e439ea52333d02be1a8
Collinb190 <collinb190@gmail.com>
2024-03-23 10:45:55 -0500

    task 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const myVarOne = 'C is fun';
+const myVarTwo = 'Python is cool';
+const myVarThree = 'JavaScript is amazing';
+
+console.log(myVarOne);
+console.log(myVarTwo);
+console.log(myVarThree);
