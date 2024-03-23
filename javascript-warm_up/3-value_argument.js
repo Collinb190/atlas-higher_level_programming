036906dcc2fb91a8408c0fea2a3f72b7bbcfcc67
Collinb190 <collinb190@gmail.com>
2024-03-23 11:30:26 -0500

    task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const length = process.argv.length;
+const myArgs = process.argv[2];
+
+if (length === 2) {
+  console.log('No argument');
+} else if (length === 3) {
+  console.log(myArgs);
+}
