79de0cd255b597e21fffe00fb2aa10f2b1ffc9a3
Collinb190 <collinb190@gmail.com>
2024-03-23 22:05:27 -0500

    task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVarOne = 'C is fun';
+const myVarTwo = 'Python is cool';
+const myVarThree = 'JavaScript is amazing';
+const myArray = [myVarOne, myVarTwo, myVarThree];
+
+for (const string of myArray) {
+  console.log(string);
+}
