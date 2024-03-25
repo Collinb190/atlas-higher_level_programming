75fa817777e7f312d46d77be3036aac6e87c112a
Collinb190 <collinb190@gmail.com>
2024-03-25 10:50:18 -0500

    task5

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h] }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
-      let row = ''
+      let row = '';
       for (let j = 0; j < this.width; j++) {
-        row += 'X'
+        row += 'X';
       }
       console.log(row);
     }
