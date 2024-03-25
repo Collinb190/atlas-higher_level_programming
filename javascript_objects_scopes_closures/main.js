ee70cb6bb54e27bdd7392150a8e0c2356d7b0b24
Collinb190 <collinb190@gmail.com>
2024-03-25 11:05:08 -0500

    task5

--- javascript_objects_scopes_closures/main.js
+++ javascript_objects_scopes_closures/main.js
@@ -2,7 +2,13 @@
 const Rectangle = require('./4-rectangle');
 
 const r1 = new Rectangle(2, 3);
+console.log('Normal:');
 r1.print();
 
-const r2 = new Rectangle(10, 5);
-r2.print();
+console.log('Double:');
+r1.double();
+r1.print();
+
+console.log('Rotate:');
+r1.rotate();
+r1.print()
